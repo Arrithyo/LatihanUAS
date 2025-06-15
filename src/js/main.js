e171af6541402fb8341282ad304a2959af07ddcd
Arrithyo <tayok282@gmail.com>
2025-06-15 23:19:04 +0700

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// menu
+const beranda = document.getElementById('home');
+const tentang = document.getElementById('about');
+const menuu = document.getElementById('menu');
+const kontak = document.getElementById('contact');
+
+// NOTE : coba pake getId utk ubh style
